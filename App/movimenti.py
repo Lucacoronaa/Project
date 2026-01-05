f75a205ac6a6243519a19184167b0f05b8c2e382
Lucacoronaa <luca.corona0086@gmail.com>
2026-01-05 19:35:24 +0100

    commit movimenti

--- App/movimenti.py
+++ App/movimenti.py
@@ -1,11 +1,14 @@
 # APP/routes/movimenti.py
-from fastapi import APIRouter, Form, Request
+from fastapi import APIRouter, Form, Request, HTTPException
 from sqlalchemy import text
 from .db import engine
 from .auth import get_user_id
 
 router = APIRouter(prefix="/api", tags=["movimenti"])
 
+
+
+#Fa solo vedere elenco dei movimenti -- SELECT
 @router.get("/movimenti")
 def list_movimenti(request: Request, limit: int = 50):
     uid = get_user_id(request)
@@ -19,6 +22,9 @@ def list_movimenti(request: Request, limit: int = 50):
         """), {"uid": uid, "limit": limit}).fetchall()
     return [dict(r._mapping) for r in rows]
 
+
+
+# Fa inserire un movimento nuovo -- INSERT
 @router.post("/movimenti")
 def create_movimento(
     request: Request,
@@ -49,3 +55,21 @@ def create_movimento(
             """), {"user_id": uid, "datestate": occurred_at, "descrizione": descrizione, "amount": amount, "subcategoryid": subcategoryid}).fetchone()
 
     return {"status": "ok", "id": row[0]}
+
+
+
+@router.delete("/movimenti/{mov_id}")
+def delete_movimento(request: Request, mov_id: int):
+
+    user_id = get_user_id(request)
+
+    with engine.begin() as conn:
+        row = conn.execute(text("""
+                                DELETE FROM movimenti
+                                where id = :id and user_id = :user_id
+                                returning id;""" ), {"id": mov_id, "user_id": user_id}).fetchone()
+    
+        if not row:
+            raise HTTPException(status_code=400, detail = "Movimento non trovato")
+        
+        return {"ok": True, "delete_id": row[0]}
